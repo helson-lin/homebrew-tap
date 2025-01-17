487e2866a4b3b22e8b037a37991e6b817aceb8ee
jlhe15 <>
2025-01-17 16:45:09 +0800

    fix: install failed

--- doke.rb
+++ doke.rb
@@ -1,12 +1,20 @@
 class Doke < Formula
   desc "A Docker container CLI tool built with Go"
   homepage "https://github.com/helson-lin/doke"
-  url "https://github.com/helson-lin/doke/releases/download/v0.0.1/doke-v0.0.1-darwin-amd64.tar.gz"
-  sha256 ""
   version "v0.0.1"
 
+  if Hardware::CPU.arm?
+    url "https://github.com/helson-lin/doke/releases/download/v0.0.1/doke-v0.0.1-darwin-arm64.tar.gz"
+    sha256 "29292a32d45222dfed7a0f9d8ff0d38f4756d3b32813e40b80abfeb42450f01e" # 替换为实际的 arm64 SHA-256 值
+  elsif Hardware::CPU.intel?
+    url "https://github.com/helson-lin/doke/releases/download/v0.0.1/doke-v0.0.1-darwin-amd64.tar.gz"
+    sha256 "fbf1a0a50569d627e5bafb54a379140ad6a90c64472787458da9ccb40fbc5111" # 替换为实际的 x86_64 SHA-256 值
+  end
+
   def install
-    bin.install "doke"
+    Dir.glob("doke-*").each do |file|
+      bin.install file => "doke"  # 将文件重命名为 doke
+    end
   end
 
   test do
