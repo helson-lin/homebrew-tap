fb626b7b9282d8f996a28495a7627966b52a95f9
helson-lin <84565447+helson-lin@users.noreply.github.com>
2025-01-17 18:34:23 +0800

    Update doke.rb

--- doke.rb
+++ doke.rb
@@ -5,7 +5,7 @@ class Doke < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/helson-lin/doke/releases/download/v0.0.4-beta/doke-v0.0.4-beta-darwin-arm64.tar.gz"
-    sha256 "2703a59566d220113b417d44bc297df7f63af4ce12affd3d4905587736d4d5d9" # 替换为实际的 arm64 SHA-256 值
+    sha256 "e85194b49cfaea51e071d056cd90d708c2a418dec48196f2b37a0b0ed0c09656" # 替换为实际的 arm64 SHA-256 值
   elsif Hardware::CPU.intel?
     url "https://github.com/helson-lin/doke/releases/download/v0.0.4-beta/doke-v0.0.4-beta-darwin-arm64.tar.gz"
     sha256 "2703a59566d220113b417d44bc297df7f63af4ce12affd3d4905587736d4d5d9" # 替换为实际的 x86_64 SHA-256 值
