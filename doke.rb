b2ce6558a846f7afaac31cb1f915afc5f112d571
helsonlin <helsonlin@163.com>
2025-01-17 06:49:26 +0000

    Update doke to v0.0.1

--- doke.rb
+++ doke.rb
@@ -1,9 +1,9 @@
 class Doke < Formula
   desc "A Docker container CLI tool built with Go"
   homepage "https://github.com/helson-lin/doke"
-  url "https://github.com/helson-lin/doke/releases/download/v1.0.0/doke_0.0.1_darwin_amd64.tar.gz"
-  sha256 "your-sha256-checksum"
-  version "1.0.0"
+  url "https://github.com/helson-lin/doke/releases/download/v0.0.1/doke_v0.0.1_darwin_amd64.tar.gz"
+  sha256 ""
+  version "v0.0.1"
 
   def install
     bin.install "doke"
