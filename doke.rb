13e9d5f5b33d73e30071ad366458d5b1df5ff6a3
helsonlin <helsonlin@163.com>
2025-07-03 16:01:48 +0000

    Update doke to v0.0.3

--- doke.rb
+++ doke.rb
@@ -5,10 +5,10 @@ class Doke < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/helson-lin/doke/releases/download/v0.0.3/doke-v0.0.3-darwin-arm64.tar.gz"
-    sha256 "706b8194515c64781b2de48aa8e04d517982b74fe92390e0c385ecd39b61e4c3" # 替换为实际的 arm64 SHA-256 值
+    sha256 "833a0635e88c0c3fbce5455640c912a7ed10bc603c7b710bbffdb99c85c1aede" # 替换为实际的 arm64 SHA-256 值
   elsif Hardware::CPU.intel?
     url "https://github.com/helson-lin/doke/releases/download/v0.0.3/doke-v0.0.3-darwin-arm64.tar.gz"
-    sha256 "f7930121271b611348323199ad93ac31bad21c55888b362b1fb773ede56db162" # 替换为实际的 x86_64 SHA-256 值
+    sha256 "226ddf608002cf08f9c23c7169491e3d944da6d33b179e128cfcae8642811a10" # 替换为实际的 x86_64 SHA-256 值
   end
 
   def install
