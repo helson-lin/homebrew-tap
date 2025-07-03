8e22b3b4988313e561986b51d255d20d0b1021ce
helsonlin <helsonlin@163.com>
2025-07-03 11:47:52 +0000

    Update doke to v0.0.2

--- doke.rb
+++ doke.rb
@@ -1,14 +1,14 @@
 class Doke < Formula
   desc "A Docker container CLI tool built with Go"
   homepage "https://github.com/helson-lin/doke"
-  version "v0.0.4-beta"
+  version "v0.0.2"
 
   if Hardware::CPU.arm?
-    url "https://github.com/helson-lin/doke/releases/download/v0.0.4-beta/doke-v0.0.4-beta-darwin-arm64.tar.gz"
-    sha256 "e85194b49cfaea51e071d056cd90d708c2a418dec48196f2b37a0b0ed0c09656" # 替换为实际的 arm64 SHA-256 值
+    url "https://github.com/helson-lin/doke/releases/download/v0.0.2/doke-v0.0.2-darwin-arm64.tar.gz"
+    sha256 "9b0c7c91d5aba919fbd62080eb5bdf71ce15cfa5794806dcc8abd8796e234b79" # 替换为实际的 arm64 SHA-256 值
   elsif Hardware::CPU.intel?
-    url "https://github.com/helson-lin/doke/releases/download/v0.0.4-beta/doke-v0.0.4-beta-darwin-arm64.tar.gz"
-    sha256 "2703a59566d220113b417d44bc297df7f63af4ce12affd3d4905587736d4d5d9" # 替换为实际的 x86_64 SHA-256 值
+    url "https://github.com/helson-lin/doke/releases/download/v0.0.2/doke-v0.0.2-darwin-arm64.tar.gz"
+    sha256 "9b0c7c91d5aba919fbd62080eb5bdf71ce15cfa5794806dcc8abd8796e234b79" # 替换为实际的 x86_64 SHA-256 值
   end
 
   def install
