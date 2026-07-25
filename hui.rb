ccf5a9f9a08795596477a84f23d62d442ede873b
helsonlin <helsonlin@163.com>
2026-07-25 21:32:32 +0800

    fix: only ship root hui.rb like doke/of (drop Formula/ duplicate)
    Duplicate class Hui in root + Formula/ can make brew skip the formula entirely.

--- hui.rb
+++ hui.rb
@@ -2,9 +2,8 @@ class Hui < Formula
   desc "Markdown to PNG/PDF/HTML converter with multi-theme support"
   homepage "https://github.com/helson-lin/hui"
   version "v1.0.0"
-  license "MIT"
 
-  if OS.mac?
+  on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/helson-lin/hui/releases/download/v1.0.0/hui-v1.0.0-darwin-arm64.tar.gz"
       sha256 "5f6099e74741da8372587003fea9482108643c2e8d5c054d09ad66626abf2907"
@@ -29,8 +28,6 @@ class Hui < Formula
   end
 
   test do
-    (testpath/"sample.md").write("# hello\n\nfrom hui\n")
-    system "#{bin}/hui", "convert", "sample.md", "-f", "html", "-o", "sample.html"
-    assert_predicate testpath/"sample.html", :exist?
+    system "#{bin}/hui", "--version"
   end
 end
